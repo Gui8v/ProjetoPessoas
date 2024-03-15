7fa3510d768c5905280e33c3b77d5de51e370019
Guilherme <gui8v.rocha8@hotmail.com>
2024-03-15 15:18:51 -0300

    Inicialização do Projeto

---
+++ src/main/java/com/ProjetoPessoas/ProjetoPessoasApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetoPessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPessoasApplication.class, args);
+	}
+
+}
