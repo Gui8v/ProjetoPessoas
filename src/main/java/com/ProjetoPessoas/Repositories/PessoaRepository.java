2e9ca2b7f33c42837f685adfd53b80008cafd7b9
Guilherme <gui8v.rocha8@hotmail.com>
2024-03-16 15:04:18 -0300

    Criação da camada de service e end points findAll e findById

--- src/main/java/com/ProjetoPessoas/Repositories/PessoaRepository.java
+++ src/main/java/com/ProjetoPessoas/Repositories/PessoaRepository.java
@@ -1,6 +1,5 @@
 package com.ProjetoPessoas.Repositories;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ProjetoPessoas.entities.Pessoa;
